49e439f7e4952b43b026cbd6349e4fb63e039fcc
Newton Ombese <newtonombese1@gmail.com>
2023-08-25 05:07:28 +0300

    Include component to be rendered by the App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import UsersView from "./store/users/usersView";
+
 function App() {
   return (
     <div className="App">
-      
+      <h1>User List</h1>
+      <UsersView />
     </div>
   );
 }
